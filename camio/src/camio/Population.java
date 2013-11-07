b401db657db86b1d605220bdff9a31287654f36e
josepcastro <castrosalvadojosep@gmail.com>
2013-11-07 12:49:55 +0100

    1.1

--- camio/src/camio/Population.java
+++ camio/src/camio/Population.java
@@ -1,7 +1,4 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package camio;
 
 import java.util.Scanner;       //importa llibreries per poder llegir de teclat
