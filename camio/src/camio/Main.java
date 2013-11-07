7b8788d1ae66b2a9773caa03d882ba2da7c02407
josepcastro <castrosalvadojosep@gmail.com>
2013-11-07 09:45:34 +0100

    tonteria

--- camio/src/camio/Main.java
+++ camio/src/camio/Main.java
@@ -1,19 +1,12 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package camio;
 
-/**
- *
- * @author josep
- */
+//importa llibreries
+import camio.Tsp_ga;
+
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
 }
