375e9580ae1da8abcbc9b593bfbec0aa646dd107
polamoros <pol.beat12@gmail.com>
2013-11-13 12:21:47 +0100

    2

---
+++ projecte_prop/src/projecte_prop/Relation.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projecte_prop;
+
+/**
+ *
+ * @author helewt
+ */
+public class Relation {
+    int id_elem1;
+    int id_elem2;
+    int distancia;
+    
+    
+    public Relation(int id1, int id2, int dist){
+        id_elem1 = id1;
+        id_elem2 = id2;
+        distancia = dist;
+        
+    }
+    
+    public int relation_with(int id){
+        
+        
+        
+    }
+    
+    
+    
+}
