6bac46ccf1117a261a7c8aa4a9a47a948489d20c
carlosep93 <carlos.e.p93@gmail.com>
2013-11-13 19:33:30 +0100

    a

--- projecte_prop/src/projecte_prop/twoapp.java
+++ projecte_prop/src/projecte_prop/twoapp.java
@@ -3,23 +3,11 @@
  * and open the template in the editor.
  */
 package projecte_prop;
-import java.util.List;
+
 /**
  *
  * @author joanbarrosogarrido
  */
-public class twoapp {
+public class Twoapp {
     
-    public static Tour () {
-        int [][] M = getMatrix () {
-        
-        }
-        int [][] eulerian_graph = prim(M);
-        List eulerian_tour = epath(eulerian_graph);
-        
-    }
-    
-    private  List epath(int [][] M) {
-        
-    } 
 }
