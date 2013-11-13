5b2ab842c48c8d622813ae574b4685ad6f46e334
josepcastro <castrosalvadojosep@gmail.com>
2013-11-13 23:13:52 +0100

    amb el main

--- projecte_prop/src/Drivers_class/DriverClass_Tour.java
+++ projecte_prop/src/Drivers_class/DriverClass_Tour.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
  */
 public class DriverClass_Tour {
     
-    public static void main(){
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         
         Tour ruta = new Tour();
