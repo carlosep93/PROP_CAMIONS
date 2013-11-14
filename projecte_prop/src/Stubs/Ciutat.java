7934e3cd76d98082fbf1bae451a62e1a792757e5
josepcastro <castrosalvadojosep@gmail.com>
2013-11-14 17:07:15 +0100

    stubs ciutat

---
+++ projecte_prop/src/Stubs/Ciutat.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Stubs;
+
+/**
+ *
+ * @author josep
+ */
+public class Ciutat {
+    Relations rel;
+    
+    public Ciutat(){
+        rel = new Relations();
+    }
+    
+    public Relations getRelation(){
+        return rel;
+    }
+}
