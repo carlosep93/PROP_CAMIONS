ac6d342d6c7662b1fa8e24171ad191e0fa3c11ce
carlosep93 <carlos.e.p93@gmail.com>
2013-11-17 13:55:02 +0100

    sa

--- projecte_prop/src/projecte_prop/Relations.java
+++ projecte_prop/src/projecte_prop/Relations.java
@@ -51,13 +51,23 @@ public class Relations {
         return cost;
     }
     
+    
+    /** @brief  Esborra un element de la Relations
+        \pre    La Relation ha de contenir elements    
+        \post   L'element indicat per el parametre id es marca com esborrat
+    */
     public void erase(int id){
+        
         int n = a.get(id).size();
         a.get(id).remove(n-1);
         a.get(id).add(-1);
     }
-    
-      public void addElement(int id) {
+   
+    /** @brief  Agefeix un element a la Relation
+        \pre    La Relation ha de estar inicialitzada    
+        \post   La Relation conté les rela
+    */    
+    public void addElement(int id) {
         Scanner in = new Scanner(System.in);
         int x;
         List<Integer> aux = new ArrayList<Integer>();
@@ -79,17 +89,49 @@ public class Relations {
     }
     
     public int [][] toMatrix() {
-        int [][] res = new int [a.size()][a.size()];
-        for (int i=0;i<a.size();++i){
-            for (int j=0;j<a.get(i).size();++j) {
-                res[i][j] = res[j][i] = a.get(i).get(j);
+        int n = 0;
+        boolean [] b = new boolean [a.size()];
+        for (int j=0;j < a.size(); ++j) {
+            if (a.get(j).get(a.get(j).size()-1) == -1) {
+                System.out.println(j);
+                b[j] = true;
+                ++n;
             }
+        }
+        int [][] aux = new int [a.size()][a.size()];
+        for (int i=0;i<a.size()-n;++i){
+            for (int j=0;j <= i;++j) {
+                aux[i][j] = aux[j][i] = a.get(i).get(j);
+            }
+        }
+        int [][] res = new int [a.size()-n][a.size()-n];
+        int a1 = 0;
+        int a2;
+        for (int i=0;i<a.size();++i){
+            a2 = 0;
+            for (int j=0;j<=i;++j) {
+                if (!b[i] && !b[j]) {
+                    //System.out.println("i: "+i+"j: "+j );
+                    res [a1][a2] = res[a2][a1]= aux[i][j];
+                    ++a2;
+                }
+            }
+            if(!b[i]) ++a1;
         }
         return res;
     }
     
     public int size() {
         return a.size();
+    }
+    
+    public List<Integer> Actius() {
+        List<Integer> l = new ArrayList<Integer>();
+        for (int i=0;i<a.size();++i) {
+            if (a.get(i).get(a.get(i).size()-1) == 0)
+                l.add(i);
+        }
+        return l;
     }
     
     public void readRelations(int n) {
