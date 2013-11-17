b4eaba62980239324a944ba39731418ef19dc2e7
Joan <joanb@jediupc.com>
2013-11-17 13:53:37 +0100

    joan

---
+++ projecte_prop/src/Drivers_class/DrivesClass_Mst.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Drivers_class;
+
+/**
+ *
+ * @author joanbarrosogarrido
+ */
+public class DrivesClass_Mst {
+    
+}
