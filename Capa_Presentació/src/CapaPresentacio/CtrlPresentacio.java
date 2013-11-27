9f853291d42d6048aaced6dcf1d8ad2bd1306aea
polamoros <pol.beat12@gmail.com>
2013-11-27 21:00:31 +0100

    Presentació

---
+++ Capa_Presentació/src/CapaPresentacio/CtrlPresentacio.java
@@ -0,0 +1,21 @@
+package CapaPresentacio;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author helewt
+ */
+public class CtrlPresentacio {
+
+    static Boolean Genetic;
+    static Boolean Anealing;
+    
+    public static void main(String[] args) {
+        Genetic = true;
+    }
+}
