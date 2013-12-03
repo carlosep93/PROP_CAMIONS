bc5783d88446ec618b70f8f8b2efb63b052ccccf
carlosep93 <carlos.e.p93@gmail.com>
2013-12-03 14:14:32 +0100

    ctrlpresentacio

--- Capa_Presentació/src/CapaPresentacio/CtrlPresentacio.java
+++ Capa_Presentació/src/CapaPresentacio/CtrlPresentacio.java
@@ -1,5 +1,9 @@
 package CapaPresentacio;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map.Entry;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -14,11 +18,22 @@ public class CtrlPresentacio {
 
     static Boolean Genetic;
     static Boolean Anealing;
+    static List<Entry < Integer,Integer > > punts = new ArrayList <>();
+    
+    public void addPunt(int x,int y) {
+        Entry<Integer,Integer> aux = new java.util.AbstractMap.SimpleEntry<Integer, Integer>(x,y);
+        punts.add(aux);
+    }
     
     public static void main(String[] args) {
         Genetic = true;
         Vista vist = new Vista();
         
-        
+    }   
+
+    public List<Entry < Integer,Integer > > lpunts() {
+        return punts;
     }
+    
+
 }
